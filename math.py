ca7b43fdb592d6dab85c371708c5a90e6315a3ea
Aditya Sharma <aditya002212@gmail.com>
2026-04-14 18:01:51 +0530

    retrival process or mini rag

--- math.py
+++ math.py
@@ -31,3 +31,29 @@ v1 = [1, 1]
 v2 = [10, 10]
 
 print(cosine_similarity(v1, v2))  # Should be ~1 
+
+
+# retrive the info 
+# in this we make our data store in vectors and our query for which data to be fetch is also vector 
+# in retrival process query vector have compute with data vectors and we find their cosine similarity the best result will be fetch 
+# same process in rag 
+
+def retrival(query,docs):
+  best_score=-1
+  best_docs=None
+  for doc in docs:
+    score=cosine_similarity(query,docs[doc])
+    if score>best_score:
+      best_score=score
+      best_docs=doc
+  return best_docs
+
+documents = {
+   "doc1": [1, 1],   # math related
+   "doc2": [0, 1],   # biology
+   "doc3": [-1, 0],  # dance
+}
+query=[1,1]
+
+result=retrival(query,documents)
+print(result)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+    "doc3": [-1, 0],  # dance
+}
+query=[1,1]
+
+result=retrival(query,documents)
+print(result)
