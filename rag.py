27a753a861dcbcf1e671877989c716f182bf4de6
Aditya Sharma <aditya002212@gmail.com>
2026-04-22 17:40:10 +0530

    added docs embeddings

---
+++ rag.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
+
+model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+docs=['1. Capital of France - Paris',
+'2.  Capital of India - New Delhi',
+'3. Capital of Japan - Tokyo',
+'4. Capital of Germany - Berlin',
+'5. Capital of Italy - Rome']
+
+embedding_doc=model.embedding_doc(docs,dimensions=384)
+print(embedding_doc)
+
